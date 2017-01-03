fff7884f689b57cb08a4a7df7e0bbd7df5ee041b
Will Puricz <wpuricz@kpmg.com>
2017-01-03 15:39:00 -0500

    Added dummy route to verify db connection

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -12,6 +12,17 @@ drop.get { req in
     ])
 }
 
+// Route with raw DB connection
+drop.get("version") { request in
+	if let db = drop.database?.driver as? PostgreSQLDriver {
+			let version = try db.raw("SELECT version()")
+			return try JSON(node: version)
+		}else{
+			return "no db connection"
+		}
+
+}
+
 drop.resource("posts", PostController())
 
 drop.run()
