f46cbd83c815c1993118edd11147022a95a62a15
Will Puricz <wpuricz@kpmg.com>
2017-01-03 15:20:41 -0500

    Initial Commit for vapor template with Postgresql connected

---
+++ Sources/App/main.swift
@@ -0,0 +1,17 @@
+import Vapor
+import VaporPostgreSQL
+
+
+let drop = Droplet(
+    providers: [VaporPostgreSQL.Provider.self]
+)
+
+drop.get { req in
+    return try drop.view.make("welcome", [
+    	"message": drop.localization[req.lang, "welcome", "title"]
+    ])
+}
+
+drop.resource("posts", PostController())
+
+drop.run()
