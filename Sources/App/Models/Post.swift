33a0f3403ec7d73aa1b88f07ef5e4add3ae93dcc
Will Puricz <wpuricz@kpmg.com>
2017-01-27 17:04:09 -0500

    Added authentication to template

--- Sources/App/Models/Post.swift
+++ Sources/App/Models/Post.swift
@@ -37,9 +37,15 @@ extension Post {
 extension Post: Preparation {
     static func prepare(_ database: Database) throws {
         //
+        try database.create("posts") { posts in
+            posts.id()
+            posts.string("content")
+
+        }
     }
 
     static func revert(_ database: Database) throws {
         //
+        try database.delete("posts")
     }
 }
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,10 +1,23 @@
 import Vapor
+import Auth
+import HTTP
+import Cookies
+import Turnstile
+import TurnstileCrypto
+import TurnstileWeb
+import Fluent
+import Foundation
 import VaporPostgreSQL
 
 
 let drop = Droplet(
     providers: [VaporPostgreSQL.Provider.self]
 )
+
+drop.preparations.append(User.self)
+drop.preparations.append(Post.self)
+drop.middleware.append(AuthMiddleware<User>())
+drop.middleware.append(TrustProxyMiddleware())
 
 drop.get { req in
     return try drop.view.make("welcome", [
@@ -21,6 +34,31 @@ drop.get("version") { request in
 			return "no db connection"
 		}
 
+}
+
+drop.group("api") { api in
+    api.group("v1") { v1 in
+        
+        let usersController = UsersController()
+        
+        // Registration
+        v1.post("register", handler: usersController.register)
+        
+        // Log In
+        v1.post("login", handler: usersController.login)
+
+        // Log Out
+        v1.post("logout", handler: usersController.logout)
+        
+        // Secured Endpoints
+        let protect = ProtectMiddleware(error: Abort.custom(status: .unauthorized, message: "Unauthorized"))
+        v1.group(BasicAuthMiddleware(), protect) { secured in
+            
+            secured.get("me", handler: usersController.me) 
+            secured.resource("posts",PostController())           
+
+        }
+    }
 }
 
 drop.resource("posts", PostController())
